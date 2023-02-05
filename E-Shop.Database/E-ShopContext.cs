8db57e8bde365f56053586ef4b2079efb30463b3
Usama Jawaid <shaikhusama890@gmail.com>
2023-02-05 21:52:20 +0500

    Create Database

---
+++ E-Shop.Database/E-ShopContext.cs
@@ -0,0 +1,21 @@
+﻿using E_Shop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Shop.Database
+{
+    class E_ShopContext : DbContext
+    {
+        public E_ShopContext() : base("E-ShopConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> products { get; set; }
+    }
+}
---
+++ E-Shop.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace E_Shop.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<E_Shop.Database.E_ShopContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(E_Shop.Database.E_ShopContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

7a0fd3a0b3018868058e32fe9408c33911feeab5
Usama Jawaid <shaikhusama890@gmail.com>
2023-02-05 20:00:25 +0500

    Initialize and made entitis

---
+++ E-Shop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Shop.Web.Startup))]
+namespace E_Shop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
